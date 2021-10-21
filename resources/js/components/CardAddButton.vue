a0957f2a00e33703c22c2748966773186a51ca3e
Nurseyit <programmer.kg@mail.ru>
2021-10-21 21:55:29 +0600

    add to card

--- resources/js/components/CardAddButton.vue
+++ resources/js/components/CardAddButton.vue
@@ -1,5 +1,34 @@
 <template>
-    <div class="rounded-sm p-2 text-gray-600 cursor-pointer hover:bg-gray-400 hover:text-gray-800 text-sm">
-        Add new Card 11
-    </div>
+    <button @click="addCard" class="rounded-sm p-2 text-gray-600 cursor-pointer hover:bg-gray-400 hover:text-gray-800 text-sm">
+        Add new Card
+    </button>
 </template>
+
+<script>
+    import CardAdd from '../graphql/CardAdd.gql';
+    import BoardQuery from '../graphql/BoardWithListsAndCards.gql';
+    export default {
+        methods: {
+            addCard() {
+                this.$apollo.mutate({
+                    mutation: CardAdd,
+                    variables: {
+                        title: 'Added throud mutation',
+                        listId: 1,
+                        order: 1
+                    },
+                    update(store, { data: { cardAdd }}) {
+                        const data = store.readQuery({
+                            query: BoardQuery,
+                            variables: {id: 1}
+                        });
+
+                        data.board.lists.find(list => (list.id = 1)).cards.push(cardAdd);
+                        store.writeQuery({ query: BoardQuery, data });
+                    }
+                })
+
+            }
+        }
+    }
+</script>
